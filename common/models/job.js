c4858283195d5f2a14e64bf40d846fcef32cf22a
yaswanth koneri <knyaswanth@iitrpr.ac.in>
2018-09-22 15:40:43 +0530

    formidable

--- common/models/job.js
+++ common/models/job.js
@@ -1,13 +1,15 @@
 'use strict';
 
-var formidable = require('formidable');
+var IncomingForm = require('formidable').IncomingForm;
 
 module.exports = function(Job) {
     Job.beforeRemote('create',function(ctx,job,next){
         var req = ctx.req
-        console.log(req)
-        var form = new formidable.IncomingForm();
+        var form = new IncomingForm();
         form.parse(req,function(err,fields,files){
+            if (err) throw err;
+            console.log( typeof files.file)
+            console.log(files.file)
             var oldpath = files.filetoupload.path
             var newpath = `../data/${ctx.body.name}`;
             fs.rename(oldpath,newpath,function(err){
