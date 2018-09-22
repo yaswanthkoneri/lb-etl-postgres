ac97c0533aac1c545b7fe3497f39f028cd63cf17
yaswanth koneri <knyaswanth@iitrpr.ac.in>
2018-09-22 11:34:59 +0530

    test

--- common/models/job.js
+++ common/models/job.js
@@ -1,5 +1,19 @@
 'use strict';
 
-module.exports = function(Job) {
+var formidable = require('formidable');
 
+module.exports = function(Job) {
+    Job.beforeRemote('create',function(ctx,job,next){
+        var req = ctx.req
+        console.log(req)
+        var form = new formidable.IncomingForm();
+        form.parse(req,function(err,fields,files){
+            var oldpath = files.filetoupload.path
+            var newpath = `../data/${ctx.body.name}`;
+            fs.rename(oldpath,newpath,function(err){
+                if (err) throw err;
+                next();
+            })
+        })
+    })
 };
