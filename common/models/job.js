c9b30acf688f8920196bacc727e9f9c6cec9396f
yaswanth koneri <knyaswanth@iitrpr.ac.in>
2018-09-23 15:23:59 +0530

    fileupload route added

--- common/models/job.js
+++ common/models/job.js
@@ -1,21 +1,5 @@
 'use strict';
 
-var IncomingForm = require('formidable').IncomingForm;
 
 module.exports = function(Job) {
-    Job.beforeRemote('create',function(ctx,job,next){
-        var req = ctx.req
-        var form = new IncomingForm();
-        form.parse(req,function(err,fields,files){
-            if (err) throw err;
-            console.log( typeof files.file)
-            console.log(files.file)
-            var oldpath = files.filetoupload.path
-            var newpath = `../data/${ctx.body.name}`;
-            fs.rename(oldpath,newpath,function(err){
-                if (err) throw err;
-                next();
-            })
-        })
-    })
 };
--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var IncomingForm = require('formidable').IncomingForm;
+var bodyParser = require('body-parser');
+var fs = require('fs');
 
 var app = module.exports = loopback();
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.start = function() {
   // start the web server
   return app.listen(function() {
@@ -27,3 +30,27 @@ boot(app, __dirname, function(err) {
   if (require.main === module)
     app.start();
 });
+
+app.route('/api/jobs/fileupload').post(function(req,res){
+  var form = new IncomingForm();
+  form.parse(req,function(err,fields,files){
+      if (err) throw err;
+      console.log( typeof files.file)
+      console.log(files)
+      // console.log("BODY",req.body,fields)
+      var data = JSON.stringify(files.file)
+      var b = JSON.parse(data)
+      console.log(b)
+      var oldpath = b.path
+      var newpath = `./data/${req.query.name}`;
+      console.log("yaswanth",newpath)
+      fs.readFile(oldpath,'utf-8',function(err,data){
+        if (err) throw err;
+        fs.writeFile (newpath, data, function(err) {
+          if (err) throw err;
+          console.log('complete');
+      });
+          // res.json({"status":"success"})
+      })
+  })
+})
